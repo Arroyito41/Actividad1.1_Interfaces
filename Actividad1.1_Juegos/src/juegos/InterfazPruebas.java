7a0f2ce7029c728d835163f5b51e7f914ecb8bf2
jarro <jarro@DESKTOP-9BMQJTA>
2022-09-30 12:31:46 +0200

    Modificacion prueba1

--- Actividad1.1_Juegos/src/juegos/InterfazPruebas.java
+++ Actividad1.1_Juegos/src/juegos/InterfazPruebas.java
@@ -17,6 +17,7 @@ import java.awt.Font;
 import javax.swing.JScrollPane;
 import java.awt.Label;
 import javax.swing.ImageIcon;
+import java.awt.Color;
 
 public class InterfazPruebas extends JFrame {
 
@@ -48,13 +49,21 @@ public class InterfazPruebas extends JFrame {
 		ArrayList <JButton> puertas = new ArrayList<>();
 		
 		
-		JButton btnPuerta1 = new JButton("Puerta 1");
+		JButton btnPuerta1 = new JButton("1");
+		btnPuerta1.setForeground(new Color(255, 255, 255));
+		btnPuerta1.setBackground(new Color(128, 0, 0));
 		puertas.add(btnPuerta1);
-		JButton btnPuerta2 = new JButton("Puerta 2");
+		JButton btnPuerta2 = new JButton("2");
+		btnPuerta2.setBackground(new Color(128, 0, 0));
+		btnPuerta2.setForeground(new Color(255, 255, 255));
 		puertas.add(btnPuerta2);
-		JButton btnPuerta3 = new JButton("Puerta 3");
+		JButton btnPuerta3 = new JButton("3");
+		btnPuerta3.setForeground(new Color(255, 255, 255));
+		btnPuerta3.setBackground(new Color(128, 0, 0));
 		puertas.add(btnPuerta3);
-		JButton btnPuerta4 = new JButton("Puerta 4");
+		JButton btnPuerta4 = new JButton("4");
+		btnPuerta4.setForeground(new Color(255, 255, 255));
+		btnPuerta4.setBackground(new Color(128, 0, 0));
 		puertas.add(btnPuerta4);
 		
 		ganador = (int)(1+Math.random()*(puertas.size()));
@@ -67,7 +76,8 @@ public class InterfazPruebas extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 10, 416, 243);
+		panel.setBackground(new Color(200, 255, 255));
+		panel.setBounds(0, 0, 447, 274);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		lblPerder.setHorizontalAlignment(SwingConstants.CENTER);
@@ -86,7 +96,7 @@ public class InterfazPruebas extends JFrame {
 		JLabel lblPrueba1 = new JLabel("Prueba 1");
 		lblPrueba1.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
 		lblPrueba1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPrueba1.setBounds(114, 0, 157, 58);
+		lblPrueba1.setBounds(114, 0, 173, 58);
 		panel.add(lblPrueba1);
 		
 		JLabel lblResultado = new JLabel("");
@@ -107,7 +117,7 @@ public class InterfazPruebas extends JFrame {
 				}
 			}
 		});
-		btnPuerta1.setBounds(10, 83, 71, 21);
+		btnPuerta1.setBounds(10, 83, 71, 103);
 		panel.add(btnPuerta1);
 		
 		btnPuerta2.addActionListener(new ActionListener() {
@@ -123,7 +133,7 @@ public class InterfazPruebas extends JFrame {
 				}
 			}
 		});
-		btnPuerta2.setBounds(114, 83, 71, 21);
+		btnPuerta2.setBounds(114, 83, 71, 103);
 		panel.add(btnPuerta2);
 		
 		btnPuerta3.addActionListener(new ActionListener() {
@@ -139,7 +149,7 @@ public class InterfazPruebas extends JFrame {
 				}
 			}
 		});
-		btnPuerta3.setBounds(229, 83, 71, 21);
+		btnPuerta3.setBounds(229, 83, 71, 103);
 		panel.add(btnPuerta3);
 		
 		btnPuerta4.addActionListener(new ActionListener() {
@@ -155,8 +165,12 @@ public class InterfazPruebas extends JFrame {
 				}
 			}
 		});
-		btnPuerta4.setBounds(335, 83, 71, 21);
+		btnPuerta4.setBounds(335, 83, 71, 103);
 		panel.add(btnPuerta4);
+		
+		JLabel lblEleccion = new JLabel("Elige una puerta");
+		lblEleccion.setBounds(164, 60, 148, 13);
+		panel.add(lblEleccion);
 		
 	}
 }
